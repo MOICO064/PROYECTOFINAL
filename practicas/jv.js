f9ee62e058e44b1cd97f64ebaa5bf08efa4269df
MOICO064 <moi.pozohuayta@gmail.com>
2021-12-20 08:58:26 -0400

    MOISES

---
+++ practicas/jv.js
@@ -0,0 +1,14 @@
+//variables
+/*let a=true;
+var edad=4;
+var peso=150.8;
+var falso=false;
+var vev=[2,2,2,3];
+var matris=[[3,15,6,1],[15,154,6,1,3]];
+
+document.write(edad);*/
+
+var nombre='moises';
+var apellido='pozo';
+//document.write(nombre+ ' '+apellido);
+document.write(nombre);
